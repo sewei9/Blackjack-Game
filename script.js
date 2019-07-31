c87f26175c23b5ae13d64a9561b0658cf2115639
Sebastian Weiss <sebastian.weiss2@ikea.com>
2019-07-31 13:01:46 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+//
+// Blackjack Game
+// by Sebastian Weiss
+//
+let card1 = "Ace of Spades",
+    card2 = "Ten of Hearts";
+
+console.log("Welcome to Blackjack");
+
+console.log("You are dealt: ");
+console.log(" " + card1);
+console.log(" " + card2);
