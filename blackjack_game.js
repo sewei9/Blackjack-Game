588b5e0d88497aac2e49f33622027f09f4150db3
Sebastian Weiss <sebastian.weiss2@ikea.com>
2019-07-31 15:30:13 +0200

    Main Game

---
+++ blackjack_game.js
@@ -0,0 +1,30 @@
+//
+// Blackjack Game
+// by Sebastian Weiss
+//
+
+let suits = ['Hearts', ' CLubs', ' Diamonds', 'Spades'];
+let values = ['Ace', 'King', 'Queen', 'Jack',
+                'Ten', 'Nine', 'Eight', ' Seven', 'Six',
+                'Five', 'Four', 'Three', 'Two',];
+
+let deck = [];
+
+for (let suitIdx=0; suitIdx < suits.length; suitsIdx++) {
+
+        for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+                deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+        }
+}
+
+for (let i = 0; i < deck.length; i++) {
+        console.log(deck[i])
+}
+
+let playerCards = [deck[0], deck[2]];
+
+console.log("Welcome to Blackjack");
+
+console.log("You are dealt: ");
+console.log(" " + playercards[0]);
+console.log(" " + playercards[1]);
