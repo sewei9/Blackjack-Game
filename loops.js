d50b885ab5ecf556ce20fb48c147b76cb9e1d743
Sebastian Weiss <sebastian.weiss2@ikea.com>
2019-07-31 15:28:16 +0200

    different type of loops

---
+++ loops.js
@@ -0,0 +1,26 @@
+let score = 1000;
+
+if (score > 1000) {
+    score = score + 100;
+}
+    else if (score === 1000) {
+        console.log("Almost");
+    }
+    else {
+         console.log("Nice try");
+    }
+
+    console.log("Score is: ", score);
+
+
+for (let i=0; i < 5; i++) {
+    console.log(i);
+}
+///////////////////////////////////////////////////////
+
+let count = 1;
+
+while (count < 3 ) {
+console.log(count);
+count++
+}
