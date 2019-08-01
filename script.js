b21335eaade6181e974ddf213a19d61301e0258e
Sebastian Weiss <sebastian.weiss2@ikea.com>
2019-08-01 12:28:55 +0200

    Removing and adding Elements

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let textArea = document.getElementById('text-area');
 let okButton = document.getElementById('ok-button');
 
+textArea.style.display = 'none';
+
 okButton.addEventListener('click', function(){
-  textArea.innerText = 'Button clicked!';
+  
+  textArea.style.display = 'block';
+  
 });
