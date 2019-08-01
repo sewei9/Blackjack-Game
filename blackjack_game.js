a5c661bae71d4e9459e02dda80ab92e5043e52ae
Sebastian Weiss <sebastian.weiss2@ikea.com>
2019-08-01 09:45:55 +0200

    Objects added to game

--- blackjack_game.js
+++ blackjack_game.js
@@ -8,23 +8,34 @@ let values = ['Ace', 'King', 'Queen', 'Jack',
                 'Ten', 'Nine', 'Eight', ' Seven', 'Six',
                 'Five', 'Four', 'Three', 'Two',];
 
-let deck = [];
-
-for (let suitIdx=0; suitIdx < suits.length; suitsIdx++) {
-
+function createDeck() {
+        let deck = [];
+        for (let suitIdx=0; suitIdx < suits.length; suitsIdx++) {
         for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
-                deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+                let card = {
+                        suit: suits[suitIdx],
+                        value: values[valueIdx]
+                };
+                        deck.push( card );
+                }
         }
+        return deck;
 }
 
-for (let i = 0; i < deck.length; i++) {
-        console.log(deck[i])
+function getCardString(card); {
+        return card.value + ' of ' + card.suit;
 }
 
-let playerCards = [deck[0], deck[2]];
+function getNextCard(); {
+        return deck.shift();
+}
+
+let deck = createDeck();
+
+let playerCards = [getNextCard(), getNextCard()];
 
 console.log("Welcome to Blackjack");
 
 console.log("You are dealt: ");
-console.log(" " + playercards[0]);
-console.log(" " + playercards[1]);
+console.log(" " + getCardString(playerCards[0]) );
+console.log(" " + getCardString(playerCards[1]) );
